251b1bff4a3ab7881d8208d0c448d9d418eeaaeb
Adam Sandor <adam.sandor@live.com>
2018-04-25 19:21:36 +0300

    basic auth doesn't apply to health endpoint

--- doom-state/src/main/java/doom/doomstate/SecurityConfig.java
+++ doom-state/src/main/java/doom/doomstate/SecurityConfig.java
@@ -19,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+                .antMatchers("/health")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic();
     }

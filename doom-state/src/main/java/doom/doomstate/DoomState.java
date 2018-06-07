7374ac4f82e7f974fb38fe0449b58dead3014056
Adam Sandor <adam.sandor@live.com>
2018-06-07 13:29:58 +0200

    optimise imports

--- doom-state/src/main/java/doom/doomstate/DoomState.java
+++ doom-state/src/main/java/doom/doomstate/DoomState.java
@@ -4,8 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.Map;
